5f0fe28151d0b0007444977a5e173dfc01ab0c25
matthew-dowdell <matthew.dowdell@hpe.com>
2022-12-03 09:56:48 +0000

    Add docs, improve error handling

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -23,6 +23,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 continue;
             }
 
+            // match chars across the groups of 3 lines
             if badges.is_empty() {
                 badges = line.chars().collect();
             } else {
@@ -30,15 +31,16 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 badges.retain(|c| filter.contains(&c));
             }
 
+            // find common chars in the 2 halves of the line
             let (first, second) = line.split_at(line.len() / 2);
 
             let chars: HashSet<char> = first.chars().collect();
             let common = second.chars().find(|c| chars.contains(&c)).ok_or("no common char found")?;
 
-            total1 += char_to_priority(common);
+            total1 += char_to_priority(common)?;
         }
 
-        total2 += char_to_priority(*badges.iter().next().unwrap());
+        total2 += char_to_priority(*badges.iter().next().ok_or("no common badge found")?)?;
         badges.clear();
     }
 
@@ -48,10 +50,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-fn char_to_priority(c: char) -> u32 {
+// Map an ASCII alphabetic char to a priority, where a-z are priorities 1-26, while A-Z are
+// priorities 27-52. Chars that are not ASCII alphabetic cause an error to be returned.
+fn char_to_priority(c: char) -> Result<u32, &'static str> {
     match c {
-        'A'..='Z' => (c as u32) - 38,
-        'a'..='z' => (c as u32) - 96,
-        _ => todo!(),
+        'A'..='Z' => Ok((c as u32) - 38),
+        'a'..='z' => Ok((c as u32) - 96),
+        _ => Err("failed to map char to priority"),
     }
 }
