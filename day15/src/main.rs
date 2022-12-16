54fbecfea7f2aa7142976c44fa99cda3aa202e61
matthew-dowdell <matthew.dowdell@hpe.com>
2022-12-16 08:56:33 +0000

    Day 15 - part 1

---
+++ day15/src/main.rs
@@ -0,0 +1,88 @@
+//! Solution for [Advent of Code 2022 - Day 15][1].
+//!
+//! [1]: https://adventofcode.com/2022/day/15
+
+// 5764145 too high
+
+mod models;
+
+use std::io::{self, BufRead, Lines};
+
+use models::Map;
+
+struct Answer {
+    part1: i32,
+    part2: u32,
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let answer = run(io::stdin().lock().lines())?;
+
+    println!("Solution 1: {}", answer.part1);
+    println!("Solution 2: {}", answer.part2);
+
+    Ok(())
+}
+
+fn run<T>(lines: Lines<T>) -> Result<Answer, Box<dyn std::error::Error>>
+where
+    T: BufRead,
+{
+    let target_row = 2_000_000;
+    // let target_row = 10;
+
+    let map = Map::from(lines)?;
+    let part1 = map.covers(target_row);
+
+    // for (key, item) in grid.iter() {
+    //     if item == &Item::Covered {
+    //         // println!("{:?} is covered", key);
+    //         part1 += 1;
+    //     }
+    // }
+
+    // should get 26
+    Ok(Answer { part1, part2: 0 })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::run;
+    use std::io::BufRead;
+
+    macro_rules! file {
+        ($file:literal) => {
+            std::fs::File::open(std::path::PathBuf::from(env!("CARGO_MANIFEST_DIR")).join($file))
+        };
+    }
+
+    macro_rules! lines {
+        ($file:literal) => {
+            file!($file).map(|f| std::io::BufReader::new(f).lines())
+        };
+    }
+
+    #[test]
+    #[ignore]
+    fn test_sample() -> Result<(), Box<dyn std::error::Error>> {
+        let lines = lines!("sample.txt")?;
+        let answer = run(lines)?;
+
+        assert_eq!(answer.part1, 0);
+        // assert_eq!(answer.part2, 0);
+
+        Ok(())
+    }
+
+    #[test]
+    #[ignore]
+    fn test_user_specific() -> Result<(), Box<dyn std::error::Error>> {
+        let lines = lines!("input.txt")?;
+        let answer = run(lines)?;
+
+        assert_eq!(answer.part1, 0);
+        // assert_eq!(answer.part2, 0);
+
+        Ok(())
+    }
+}
---
+++ day15/src/models.rs
@@ -0,0 +1,241 @@
+//! ...
+
+use std::cmp::{max, min};
+use std::collections::HashSet;
+use std::error::Error;
+use std::io::{BufRead, Lines};
+use std::str::FromStr;
+
+use itertools::Itertools;
+
+pub struct Map {
+    sensors: Vec<Sensor>,
+}
+
+impl Map {
+    ///
+    pub fn from<T>(lines: Lines<T>) -> Result<Self, Box<dyn Error>>
+    where
+        T: BufRead,
+    {
+        let mut sensors = Vec::new();
+
+        for line in lines {
+            let line = line?;
+
+            if line.is_empty() {
+                continue;
+            }
+
+            let (sensor_str, beacon_str) = line.split_once(':').ok_or("failed to split line")?;
+
+            let mut sensor = Sensor::from_str(sensor_str)?;
+            let beacon = Beacon::from_str(beacon_str)?;
+
+            sensor.closest = beacon.location;
+            sensors.push(sensor);
+        }
+
+        Ok(Self { sensors })
+    }
+
+    ///
+    pub fn covers(&self, target_row: i32) -> i32 {
+        let ranges = self.ranges(target_row);
+        println!("{:?}", ranges);
+
+        ranges.iter().fold(0, |acc, r| acc + r.len()) - self.items_on(target_row)
+    }
+
+    ///
+    pub fn ranges(&self, target_row: i32) -> Vec<Range> {
+        let mut ranges = Vec::new();
+
+        for sensor in self.sensors.iter() {
+            if let Some(range) = sensor.range(target_row) {
+                ranges.push(range);
+            }
+        }
+
+        ranges.sort_by_key(|x| x.start);
+
+        ranges
+            .into_iter()
+            .coalesce(|a, b| {
+                if a.touches(&b) {
+                    Ok(a.merge(&b))
+                } else {
+                    Err((a, b))
+                }
+            })
+            .collect()
+    }
+
+    ///
+    pub fn items_on(&self, target_row: i32) -> i32 {
+        // TODO: handle beacons being outside the range?
+        let mut total = 0;
+        let mut beacons = HashSet::new();
+
+        for sensor in self.sensors.iter() {
+            if sensor.location.y == target_row {
+                println!("sensor {:?}", sensor.location);
+                total += 1;
+            }
+
+            beacons.insert(sensor.closest);
+        }
+
+        for beacon in beacons.iter() {
+            if beacon.y == target_row {
+                println!("beacon {:?}", beacon);
+                total += 1;
+            }
+        }
+
+        total
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub enum Item {
+    Sensor,
+    Beacon,
+    Covered,
+}
+
+impl From<Item> for char {
+    fn from(item: Item) -> Self {
+        match item {
+            Item::Sensor => 'S',
+            Item::Beacon => 'B',
+            Item::Covered => '#',
+        }
+    }
+}
+
+pub struct Sensor {
+    pub location: Coordinate,
+    pub closest: Coordinate,
+}
+
+impl Sensor {
+    ///
+    pub fn range(&self, target_row: i32) -> Option<Range> {
+        let distance = self.location.manhattan_distance(self.closest);
+        let offset = (self.location.y - target_row).abs();
+
+        if distance >= offset {
+            let range = Range::from_mid_point(self.location.x, distance - offset);
+            println!(
+                "sensor in range {:?} ({}) {:?}",
+                self.location, distance, range
+            );
+            Some(range)
+        } else {
+            None
+        }
+    }
+}
+
+impl FromStr for Sensor {
+    type Err = Box<dyn Error>;
+
+    fn from_str(value: &str) -> Result<Self, Self::Err> {
+        let value = value
+            .strip_prefix("Sensor at x=")
+            .ok_or("missing expected sensor prefix")?;
+        let (x, value) = value.split_once(',').ok_or("missing expected delimiter")?;
+        let y = value.strip_prefix(" y=").ok_or("missing expected prefix")?;
+
+        let location = Coordinate::new(x.parse()?, y.parse()?);
+
+        Ok(Self {
+            location,
+            closest: Coordinate::default(),
+        })
+    }
+}
+
+pub struct Beacon {
+    pub location: Coordinate,
+}
+
+impl FromStr for Beacon {
+    type Err = Box<dyn Error>;
+
+    fn from_str(value: &str) -> Result<Self, Self::Err> {
+        let value = value
+            .strip_prefix(" closest beacon is at x=")
+            .ok_or("missing beacon expected prefix")?;
+        let (x, value) = value.split_once(',').ok_or("missing expected delimiter")?;
+        let y = value.strip_prefix(" y=").ok_or("missing expected prefix")?;
+
+        let location = Coordinate::new(x.parse()?, y.parse()?);
+
+        Ok(Self { location })
+    }
+}
+
+///
+#[derive(Clone, Copy, Debug, Default, Eq, Hash, PartialEq)]
+pub struct Coordinate {
+    pub x: i32,
+    pub y: i32,
+}
+
+impl Coordinate {
+    ///
+    pub fn new(x: i32, y: i32) -> Self {
+        Self { x, y }
+    }
+
+    ///
+    pub fn manhattan_distance(&self, other: Self) -> i32 {
+        (self.x - other.x).abs() + (self.y - other.y).abs()
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
+pub struct Vertex {
+    pub distance: i32,
+    pub location: Coordinate,
+}
+
+#[derive(Clone, Copy, Debug, Eq, PartialEq, PartialOrd, Ord)]
+pub struct Range {
+    pub start: i32,
+    pub end: i32,
+}
+
+impl Range {
+    ///
+    pub fn from_mid_point(mid_point: i32, width: i32) -> Self {
+        Self {
+            start: mid_point - width,
+            end: mid_point + width,
+        }
+    }
+
+    ///
+    pub fn touches(&self, other: &Self) -> bool {
+        self.start <= other.end && self.end >= other.start
+    }
+
+    ///
+    pub fn merge(&self, other: &Self) -> Self {
+        if !self.touches(other) {
+            todo!();
+        }
+
+        Self {
+            start: min(self.start, other.start),
+            end: max(self.end, other.end),
+        }
+    }
+
+    ///
+    pub fn len(&self) -> i32 {
+        self.end - self.start + 1
+    }
+}
